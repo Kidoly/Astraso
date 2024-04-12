0981a3c2f6a6092227ef662b05e707de5b64ec5a
Mathéo DELAUNAY <dseonay@gmail.com>
2024-04-12 09:21:15 +0200

    Mise à jour du code pour ajouter la fonctionnalité de création, connexion, modification et déconnexion de compte

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,16 +12,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Image;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-
-#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    #[Route('/user', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
@@ -29,89 +25,14 @@ class UserController extends AbstractController
         ]);
     }
 
-<<<<<<< HEAD
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
-        // Check if the current user is the owner of the user entity
-
-        return $this->render('user/show.html.twig', [
-=======
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/user/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user/new.html.twig', [
->>>>>>> origin/dev
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-public function show(User $user, FollowRepository $followRepository, PostRepository $postRepository): Response
-{
-    // Return the follow status of the current user
-
-    // Récupérer l'utilisateur actuel
-    $currentUser = $this->getUser();
-
-    // Vérifier si l'utilisateur actuel est authentifié
-    if ($currentUser) {
-        // Récupérer l'entité Follow correspondant à l'utilisateur actuel et à l'utilisateur suivi
-        $follow = $followRepository->findOneBy([
-            'following_user' => $currentUser,
-            'followed_user' => $user,
-        ]);
-
-        // Récupérer le nombre de personnes suivies par l'utilisateur du compte afficher
-        $numberOfFollowings = count($followRepository->findBy(['following_user' => $user]));
-
-        // Récupérer le nombre de personnes qui suivent l'utilisateur actuel
-        $numberOfFollowers = count($followRepository->findBy(['followed_user' => $user]));
-
-        //Réccupérer les nombre de posts de l'utilisateur
-        $numberOfPosts = count($postRepository->findBy(['user' => $user]));
-
-        //Afficher les tous les posts de l'utilisateur
-        $posts = $postRepository->findBy(['user' => $user]);
-
-        // Passer le résultat à la vue
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-            'follow' => $follow,
-            'numberOfFollowings' => $numberOfFollowings,
-            'numberOfFollowers' => $numberOfFollowers,
-            'numberOfPosts' => $numberOfPosts,
-            'posts' => $posts,
-        ]);
-    }
-    
-    return $this->render('user/show.html.twig', [
-        'user' => $user,
-    ]);
-}
-
-
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher, User $user): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -119,135 +40,119 @@ public function show(User $user, FollowRepository $followRepository, PostReposit
                 )
             );
 
-            /** @var UploadedFile $uploadedFile */
-            $uploadedFile = $form['image']->getData();
-
-            if ($uploadedFile) {
-                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
-                $newFilename = $safeFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
-
-                try {
-                    $uploadedFile->move(
-                        $this->getParameter('images_directory'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    // TODO: Handle exception
-                }
-
-                $image = new Image();
-                $image->setImage($newFilename);
-                $user->setImage($image);
-            }
-
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_index');
         }
 
-<<<<<<< HEAD
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
+        return $this->render('user/new.html.twig', [
             'form' => $form->createView(),
         ]);
-=======
+    }
+
+    #[Route('/user/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(User $user, FollowRepository $followRepository, PostRepository $postRepository): Response
+    {
+        $currentUser = $this->getUser();
+
+        if ($currentUser) {
+            $follow = $followRepository->findOneBy([
+                'following_user' => $currentUser,
+                'followed_user' => $user,
+            ]);
+
+            $numberOfFollowings = $followRepository->count(['following_user' => $user]);
+            $numberOfFollowers = $followRepository->count(['followed_user' => $user]);
+            $numberOfPosts = $postRepository->count(['user' => $user]);
+            $posts = $postRepository->findBy(['user' => $user]);
+
+            return $this->render('user/show.html.twig', [
+                'user' => $user,
+                'follow' => $follow,
+                'numberOfFollowings' => $numberOfFollowings,
+                'numberOfFollowers' => $numberOfFollowers,
+                'numberOfPosts' => $numberOfPosts,
+                'posts' => $posts,
+            ]);
+        }
+        
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/user/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher, User $user): Response
+    {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_show', ['id' => $user->getId()]);
         }
->>>>>>> origin/dev
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
 
-
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    #[Route('/user/{id}', name: 'app_user_delete', methods: ['DELETE'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        // Check if the current user is the owner of the user entity
-        if ($user !== $this->getUser()) {
-            throw $this->createAccessDeniedException('Access Denied');
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
         }
 
-<<<<<<< HEAD
-        // Set password to null to prevent EntityUserProvider from refreshing the user object
-        $user->setPassword('');
-
-        $entityManager->remove($user);
-        $entityManager->flush();
-
-        return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_user_index');
     }
 
-    # Method for following a user
-    #[Route('/{id}/follow', name: 'app_user_follow', methods: ['GET'])]
-    public function follow(User $user, EntityManagerInterface $entityManager, Follow $follow): Response
+    #[Route('/user/{id}/follow', name: 'app_user_follow', methods: ['GET'])]
+    public function follow(User $user, EntityManagerInterface $entityManager, FollowRepository $followRepository): Response
     {
-        $follow->setFollowingUser($this->getUser());
-=======
-        // Code for deleting the user
->>>>>>> origin/dev
-
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-    }
-
-    #[Route('/{id}/follow', name: 'app_user_follow', methods: ['GET'])]
-public function follow(User $user, EntityManagerInterface $entityManager, FollowRepository $followRepository): Response
-{
-    // Récupérer l'utilisateur actuel
-    $currentUser = $this->getUser();
-
-    // Vérifier si l'utilisateur actuel est authentifié
-    if (!$currentUser) {
-        // Gérer le cas où l'utilisateur n'est pas authentifié
-        // Par exemple, vous pouvez rediriger vers la page de connexion
-        $this->addFlash('warning', 'You must be logged in to follow a user.');
-        return $this->redirectToRoute('app_login');
-    }
-
-    
-
-    // Créer une nouvelle instance de Follow
-    $follow = new Follow();
-    $follow->setFollowingUser($currentUser);
-    $follow->setFollowedUser($user);
-
-    // Persister l'entité Follow
-    $entityManager->persist($follow);
-    $entityManager->flush();
-
-    return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
-}
-
-
-
-    
-
-    # Method for unfollowing a user
-    #[Route('/{id}/unfollow', name: 'app_user_unfollow', methods: ['GET'])]
-    public function unfollow(User $user, EntityManagerInterface $entityManager, FollowRepository $followRepository): Response
-
-    {
-        // Récupérer l'utilisateur actuel
         $currentUser = $this->getUser();
 
-        // Récupérer l'entité Follow correspondant à l'utilisateur actuel et à l'utilisateur suivi
+        if (!$currentUser) {
+            $this->addFlash('warning', 'You must be logged in to follow a user.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        $follow = new Follow();
+        $follow->setFollowingUser($currentUser);
+        $follow->setFollowedUser($user);
+
+        $entityManager->persist($follow);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_user_show', ['id' => $user->getId()]);
+    }
+
+    #[Route('/user/{id}/unfollow', name: 'app_user_unfollow', methods: ['GET'])]
+    public function unfollow(User $user, EntityManagerInterface $entityManager, FollowRepository $followRepository): Response
+    {
+        $currentUser = $this->getUser();
+
         $follow = $followRepository->findOneBy([
             'following_user' => $currentUser,
             'followed_user' => $user,
         ]);
 
-        // Vérifier si l'entité Follow existe
         if ($follow) {
-            // Supprimer l'entité Follow
             $entityManager->remove($follow);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_user_show', ['id' => $user->getId()]);
     }
 }
